929d5d48b31e015dab9e9d883b8d2e201252081b
yp0158 <87224070+Y0158@users.noreply.github.com>
2021-10-14 07:43:53 +0200

    def monthly_installment function is added to the code

--- mortgage_cal.py
+++ mortgage_cal.py
@@ -8,23 +8,30 @@
 # Output requirements
 
 # Variables
-H = float(input('Home Value ='))                  # H = total home Value
-Down_payment = float(input('Specify Down payment='))  # down payment is the payment that will be payied at start
-P = H - Down_payment                                 # p =  principal amount
-r = float(input('Yearly Interest rate='))         # r = yearly interest rate
-r1 = r / ( 100 * 12 )                             # r1 =  monthly interest rate
-Loan_Term = float(input('Loan_Term in years ='))  # Loan_Term is the period of total loan payment
-N = Loan_Term * 12                                # number of monthly payment for it is set to 12.
 
-# loop for calculating the the Monthly Payment
-# Formula for calculation is from the wiki.
-if r == 0:
-    c = P / N
-elif r > 0:
-    c1 = (r1 * P* ((1 + r1)**N))
-    c2 = ((1 + r1)**N - 1)
-    c = c1 / c2
+# defining the function for Emi
 
-print('Principal amount is {}'.format(round(P,0)))
-print('Total Monthly Payment is {}'.format(round(c,2)))
+def monthly_installment(home_value, down_payment, interest_rate, loan_term):
+    p = home_value - down_payment  # p =  principal amount
+    r1 = interest_rate / (100 * 12) # r1 =  monthly interest rate
+    n = loan_term * 12  # number of total payment = loan term * 12.
+    # loop for calculating the the Monthly Payment
+    # Formula for calculation is from the wiki.
+    if interest_rate == 0:
+        c = p / n
+    elif interest_rate > 0:
+        c1 = (r1 * p * ((1 + r1) ** n))
+        c2 = ((1 + r1) ** n - 1)
+        c = c1 / c2
+    return c, p, n
+
+
+home_value = float(input('Home Value ='))  # total home Value
+down_payment = float(input('Specify Down payment='))  # down payment is the payment that will be paid at start
+interest_rate = float(input('Yearly Interest rate='))  # yearly interest rate
+loan_term = float(input('Loan_Term in years ='))  # loan_Term is the period of total loan payment
+
+
+print('Principal amount is {principal_amount}'.format(principal_amount = p))
+print('Total Monthly Payment is {monthly_payment}'.format(monthly_payment = c))
 
